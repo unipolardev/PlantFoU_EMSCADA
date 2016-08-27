971e28e87d2f4471ae8116e8536b974ecd9ee82a
Unipolar Automation Technologies <uatechltd@gmail.com>
2016-08-27 16:09:19 +0600

    Add images,Complete History tab 80% . Complete left panel.

--- EMSCADA/EMSCADA/MainWindow.xaml.cs
+++ EMSCADA/EMSCADA/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WPF.Meter;
 
 namespace EMSCADA
 {
@@ -23,6 +24,8 @@ namespace EMSCADA
         public MainWindow()
         {
             InitializeComponent();
+            //energydiviceTree.Items.Add("Abbas");
+            
         }
     }
 }
